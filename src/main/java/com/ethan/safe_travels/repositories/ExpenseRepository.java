fb0d16e2b570733dac262729f78590fc3c8dbf06
Ethan Smith <EthanKSmith@gmail.com>
2023-02-15 18:21:35 -0700

    Initial Commit

---
+++ src/main/java/com/ethan/safe_travels/repositories/ExpenseRepository.java
@@ -0,0 +1,14 @@
+package com.ethan.safe_travels.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ethan.safe_travels.models.Expense;
+
+@Repository
+public interface ExpenseRepository extends CrudRepository<Expense, Long>{
+  // this method retrieves all the books from the database
+  List<Expense> findAll();
+}
